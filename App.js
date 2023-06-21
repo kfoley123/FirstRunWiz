dc22c5b12029284b6f9520c1b56a910549a96e0a
Kortney <kortney@iMac.local>
2023-06-21 16:26:58 -0300

    added skeleton for basic shape of app tab navigation

--- App.js
+++ App.js
@@ -1,12 +1,51 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Settings from "./Screens/Settings";
+import Profile from "./Screens/Profile";
+import { Ionicons } from "@expo/vector-icons";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+    const getTabBarIcon = (route, focused, color, size) => {
+        switch (route.name) {
+            case "Profile":
+                return (
+                    <Ionicons
+                        name={focused ? "md-person-sharp" : "md-person-outline"}
+                        size={size}
+                        color={color}
+                    />
+                );
+            case "Settings":
+                return (
+                    <Ionicons
+                        name={focused ? "ios-settings" : "ios-settings-outline"}
+                        size={size}
+                        color={color}
+                    />
+                );
+        }
+    };
+
     return (
-        <View style={styles.container}>
-            <Text>First Run Demo</Text>
-            <StatusBar style="auto" />
-        </View>
+        <NavigationContainer>
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                    headerShown: false,
+                    tabBarIcon: ({ focused, color, size }) =>
+                        getTabBarIcon(route, focused, color, size),
+                    tabBarInactiveTintColor: "gray",
+                    tabBarActiveTintColor: "tomato",
+                })}
+            >
+                <Tab.Screen name="Profile" component={Profile} />
+
+                <Tab.Screen name="Settings" component={Settings} />
+            </Tab.Navigator>
+        </NavigationContainer>
     );
 }
 
