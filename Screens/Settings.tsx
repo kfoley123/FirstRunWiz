1b8efa1054e5b365ab9d1f4b0407f25a3b382b66
Kortney <kortney@iMac.local>
2023-06-23 10:32:27 -0300

    added shape for Formik and Yup in Settings as well as typing, but left it as an empty object until fields are determined.

--- Screens/Settings.tsx
+++ Screens/Settings.tsx
@@ -1,15 +1,46 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+type SettingsValues = {};
+
+const SettingsSchema = Yup.object().shape({});
+
+const settingsFormValues: SettingsValues = {};
 
 export default function Settings() {
     return (
-        <View style={styles.container}>
-            <StatusBar />
-            <Text> Settings</Text>
-        </View>
+        <Formik
+            initialValues={settingsFormValues}
+            validationSchema={SettingsSchema}
+            onSubmit={(values) => console.log(values)}
+        >
+            {({
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                setFieldValue,
+                values,
+                errors,
+                touched,
+            }) => (
+                <View style={styles.container}>
+                    <StatusBar />
+                    <Text> Settings</Text>
+                </View>
+            )}
+        </Formik>
     );
 }
+
+// days of the week check box -
+//  selectdropdown -
+// check box / switch selector -
+// text input -
+// number input -
+//
 
 const styles = StyleSheet.create({
     container: {
